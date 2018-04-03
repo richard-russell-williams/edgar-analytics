b0421736c4dd3f2660df5fc72085b2d9c22ba27a
richard-russell-williams <38023456+richard-russell-williams@users.noreply.github.com>
2018-04-02 20:10:30 -0700

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
-#python ./src/sessionization.py ./input/log.csv ./input/inactivity_period.txt ./output/sessionization.txt
-
-g++ ./src/edgar.cpp -O3 -o edgar
-edgar ./input/log.csv ./input/inactivity_period.txt ./output/sessionization.txt
